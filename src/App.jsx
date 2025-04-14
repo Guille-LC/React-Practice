91faa2e27f3df7aff8eeaea507006a6a6b5ab0d9
GU1LL3_L4-C0 <guillermo96lc@hotmail.com>
2025-04-14 20:19:22 -0300

    react router dom instalado

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-import './components/ProductList'
+import './components/Catalogo'
 import './components/ItemCount'
 import './components/Navbar'
-import { ProductList } from './components/ProductList'
+import { Catalogo } from './components/Catalogo'
 import { Navbar } from './components/Navbar'
 import Greeting from './components/Greeting'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <div className='head'>
         <img src="../img/pngfind.com-green-day-logo-png-6068960.png" alt="Sweet Merch" className='logo'/>
         <h1>SWEET MERCH</h1>
       </div>
         <Navbar />
-        <Greeting greeting="¡Bienvenido a Sweet Merch!"/>
-        <ProductList />
-    </>
+          <Greeting greeting="¡Bienvenido a la tienda de Sweet Merch!"/>
+        <Routes>
+          <Route path='/' element={<Catalogo />}/>
+        </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/Catalogo.jsx
@@ -0,0 +1,11 @@
+import Products from './Products'
+import '../styles/catalogo.css'
+
+export const Catalogo = () => {
+    return (
+        <>
+            <h2 className='catalogoheader'>Catalogo completo</h2>
+            <Products />
+        </>
+    )
+}
